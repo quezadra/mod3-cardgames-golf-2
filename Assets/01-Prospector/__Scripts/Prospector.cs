08d4f47d8b944631fbd277a4fb9c7abf5acf4381
JackTesoro <jt1107@scarletmail.rutgers.edu>
2024-03-22 00:47:38 -0400

    playable round

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -91,9 +91,43 @@ public class Prospector : MonoBehaviour {
 
 
 		}
+		foreach(CardProspector tCP in tableau) 
+		{
+			foreach (int hid in tCP.slotDef.hiddenBy)
+			{
+				cp = FindCardByLayoutID(hid);
+				tCP.hiddenBy.Add(cp);
+			}
+		}
 		MoveToTarget(Draw ());
 
 		UpdateDrawPile();
+	}
+	CardProspector FindCardByLayoutID(int layoutID)
+	{
+		foreach(CardProspector tCP in tableau)
+		{
+			if (tCP.layoutID == layoutID)
+			{
+				return(tCP);
+			}
+		}
+		return(null);
+	}
+	void SetTableauFaces() 
+	{
+		foreach (CardProspector cd in tableau)
+		{
+			bool faceUp = true;
+			foreach(CardProspector cover in cd.hiddenBy)
+			{
+				if(cover.state == eCardState.tableau)
+				{
+					faceUp = false;
+				}
+			}
+			cd.faceUp = faceUp;
+		}
 	}
 	void MoveToDiscard(CardProspector cd)
 	{
@@ -155,7 +189,31 @@ public class Prospector : MonoBehaviour {
 				UpdateDrawPile();
 				break;
 			case eCardState.tableau:
+				bool validMatch = true;
+				if (!cd.faceUp) 
+				{
+					validMatch = false;
+				}
+				if (!AdjacentRank(cd, target))
+				{
+					validMatch = false;
+				}
+				if (!validMatch) return;
+				tableau.Remove(cd);
+				MoveToTarget(cd);
+				SetTableauFaces();
 				break;
 		}
+	}
+	public bool AdjacentRank(CardProspector c0, CardProspector c1) 
+	{
+		if (!c0.faceUp || !c1.faceUp) return(false);
+		if (Mathf.Abs(c0.rank - c1.rank) == 1) 
+		{
+			return(true);
+		}
+		if (c0.rank == 1 && c1.rank == 13) return(true);
+		if (c0.rank == 13 && c1.rank == 1) return(true);
+		return(false);
 	}
 }
