dcb0d4e9e0b44a4088e24fa512d492b8f373912f
JackTesoro <jt1107@scarletmail.rutgers.edu>
2024-03-21 22:09:56 -0400

    CardProspector
    A subclass of card

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -17,6 +17,7 @@ public class Prospector : MonoBehaviour {
 	[Header("Set Dynamically")]
 	public Deck					deck;
 	public Layout				layout;
+	public List<CardProspector>	drawPile;
 
 	void Awake(){
 		S = this;
@@ -35,6 +36,18 @@ public class Prospector : MonoBehaviour {
 		}
 		layout = GetComponent<Layout>();
 		layout.ReadLayout(layoutXML.text);
-	}
+		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
 
+	}
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+	{
+		List<CardProspector> lCP = new List<CardProspector>();
+		CardProspector tCP;
+		foreach(Card tCD in lCD)
+		{
+			tCP = tCD as CardProspector;
+			lCP.Add(tCP);
+		}
+		return(lCP);
+	}
 }
